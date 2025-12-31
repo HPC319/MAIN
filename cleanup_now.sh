1bb72616d379a5894c2b4d2b097aca43c275122f
Henry Herrera <projects@henryscontracting.com>
2025-12-30 23:28:43 -0600

    chore: enforce immutability - remove temp scripts and artifacts

---
+++ cleanup_now.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/henryherrera/MAIN
+rm -f check_and_push.sh check_status.sh ci_fix_loop.sh commit_and_push.sh execute_task.sh git_commands.sh quick_check.sh run_all_gates.sh run_gates.sh test_gate1.sh verify_fixes.sh
+rm -f delete_temp_files.sh
+echo "All temporary files deleted successfully!"
---
+++ execute_deletion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /Users/henryherrera/MAIN
+
+# Remove the temporary script files
+rm check_and_push.sh
+rm check_status.sh
+rm ci_fix_loop.sh
+rm commit_and_push.sh
+rm execute_task.sh
+rm git_commands.sh
+rm quick_check.sh
+rm run_all_gates.sh
+rm run_gates.sh
+rm test_gate1.sh
+rm verify_fixes.sh
+
+echo "All files deleted successfully"
