e99517e9f5f85ca1944b440ae63221be1893ceea
Henry Herrera <projects@henryscontracting.com>
2025-12-30 23:33:44 -0600

    chore: purge non-constitutional helper scripts (immutability enforcement)

--- git_commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/henryherrera/MAIN
-git add .github/workflows
-git commit -m "fix(ci): close constitutional gate failure"
-git push
--- test_gate1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/henryherrera/MAIN
-npm run gatekeeper:dependencies 2>&1
